c36cb8b4d9e886ebcbdc1ced293dac81bf3b1a94
Dashan <605465435@qq.com>
2016-07-25 15:22:07 +0800

    首次上传

---
+++ Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace PrizeType
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cb0 = new System.Windows.Forms.CheckBox();
+            this.cb1 = new System.Windows.Forms.CheckBox();
+            this.cb2 = new System.Windows.Forms.CheckBox();
+            this.cb3 = new System.Windows.Forms.CheckBox();
+            this.cb7 = new System.Windows.Forms.CheckBox();
+            this.cb6 = new System.Windows.Forms.CheckBox();
+            this.cb5 = new System.Windows.Forms.CheckBox();
+            this.cb4 = new System.Windows.Forms.CheckBox();
+            this.cb11 = new System.Windows.Forms.CheckBox();
+            this.cb10 = new System.Windows.Forms.CheckBox();
+            this.cb9 = new System.Windows.Forms.CheckBox();
+            this.cb8 = new System.Windows.Forms.CheckBox();
+            this.cb14 = new System.Windows.Forms.CheckBox();
+            this.cb13 = new System.Windows.Forms.CheckBox();
+            this.cb12 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cb0
+            // 
+            this.cb0.AutoSize = true;
+            this.cb0.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.cb0.Location = new System.Drawing.Point(39, 242);
+            this.cb0.Name = "cb0";
+            this.cb0.Size = new System.Drawing.Size(19, 19);
+            this.cb0.TabIndex = 0;
+            this.cb0.Text = "0";
+            this.cb0.UseVisualStyleBackColor = true;
+            // 
+            // cb1
+            // 
+            this.cb1.AutoSize = true;
+            this.cb1.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.cb1.Location = new System.Drawing.Point(39, 217);
+            this.cb1.Name = "cb1";
+            this.cb1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.cb1.Size = new System.Drawing.Size(19, 19);
+            this.cb1.TabIndex = 1;
+            this.cb1.Text = "1";
+            this.cb1.UseVisualStyleBackColor = true;
+            // 
+            // cb2
+            // 
+            this.cb2.AutoSize = true;
+            this.cb2.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.cb2.Location = new System.Drawing.Point(39, 192);
+            this.cb2.Name = "cb2";
+            this.cb2.Size = new System.Drawing.Size(19, 19);
+            this.cb2.TabIndex = 2;
+            this.cb2.Text = "2";
+            this.cb2.UseVisualStyleBackColor = true;
+            // 
+            // cb3
+            // 
+            this.cb3.AutoSize = true;
+            this.cb3.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.cb3.Location = new System.Drawing.Point(78, 241);
+            this.cb3.Name = "cb3";
+            this.cb3.Size = new System.Drawing.Size(19, 19);
+            this.cb3.TabIndex = 3;
+            this.cb3.Text = "3";
+            this.cb3.UseVisualStyleBackColor = true;
+            // 
+            // cb7
+            // 
+            this.cb7.AutoSize = true;
+            this.cb7.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.cb7.Location = new System.Drawing.Point(117, 217);
+            this.cb7.Name = "cb7";
+            this.cb7.Size = new System.Drawing.Size(19, 19);
+            this.cb7.TabIndex = 8;
+            this.cb7.Text = "7";
+            this.cb7.UseVisualStyleBackColor = true;
+            // 
+            // cb6
+            // 
+            this.cb6.AutoSize = true;
+            this.cb6.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.cb6.Location = new System.Drawing.Point(117, 242);
+            this.cb6.Name = "cb6";
+            this.cb6.Size = new System.Drawing.Size(19, 19);
+            this.cb6.TabIndex = 7;
+            this.cb6.Text = "6";
+            this.cb6.UseVisualStyleBackColor = true;
+            // 
+            // cb5
+            // 
+            this.cb5.AutoSize = true;
+            this.cb5.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.cb5.Location = new System.Drawing.Point(78, 192);
+            this.cb5.Name = "cb5";
+            this.cb5.Size = new System.Drawing.Size(19, 19);
+            this.cb5.TabIndex = 6;
+            this.cb5.Text = "5";
+            this.cb5.UseVisualStyleBackColor = true;
+            // 
+            // cb4
+            // 
+            this.cb4.AutoSize = true;
+            this.cb4.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.cb4.Location = new System.Drawing.Point(78, 217);
+            this.cb4.Name = "cb4";
+            this.cb4.Size = new System.Drawing.Size(19, 19);
+            this.cb4.TabIndex = 5;
+            this.cb4.Text = "4";
+            this.cb4.UseVisualStyleBackColor = true;
+            // 
+            // cb11
+            // 
+            this.cb11.AutoSize = true;
+            this.cb11.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.cb11.Location = new System.Drawing.Point(156, 192);
+            this.cb11.Name = "cb11";
+            this.cb11.Size = new System.Drawing.Size(27, 19);
+            this.cb11.TabIndex = 13;
+            this.cb11.Text = "11";
+            this.cb11.UseVisualStyleBackColor = true;
+            // 
+            // cb10
+            // 
+            this.cb10.AutoSize = true;
+            this.cb10.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.cb10.Location = new System.Drawing.Point(156, 217);
+            this.cb10.Name = "cb10";
+            this.cb10.Size = new System.Drawing.Size(27, 19);
+            this.cb10.TabIndex = 12;
+            this.cb10.Text = "10";
+            this.cb10.UseVisualStyleBackColor = true;
+            // 
+            // cb9
+            // 
+            this.cb9.AutoSize = true;
+            this.cb9.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.cb9.Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.cb9.Location = new System.Drawing.Point(160, 241);
+            this.cb9.Name = "cb9";
+            this.cb9.Size = new System.Drawing.Size(19, 19);
+            this.cb9.TabIndex = 11;
+            this.cb9.Text = "9";
+            this.cb9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.cb9.UseVisualStyleBackColor = true;
+            // 
+            // cb8
+            // 
+            this.cb8.AutoSize = true;
+            this.cb8.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.cb8.Location = new System.Drawing.Point(117, 192);
+            this.cb8.Name = "cb8";
+            this.cb8.Size = new System.Drawing.Size(19, 19);
+            this.cb8.TabIndex = 10;
+            this.cb8.Text = "8";
+            this.cb8.UseVisualStyleBackColor = true;
+            // 
+            // cb14
+            // 
+            this.cb14.AutoSize = true;
+            this.cb14.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.cb14.Location = new System.Drawing.Point(198, 192);
+            this.cb14.Name = "cb14";
+            this.cb14.Size = new System.Drawing.Size(27, 19);
+            this.cb14.TabIndex = 17;
+            this.cb14.Text = "14";
+            this.cb14.UseVisualStyleBackColor = true;
+            // 
+            // cb13
+            // 
+            this.cb13.AutoSize = true;
+            this.cb13.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.cb13.Location = new System.Drawing.Point(198, 217);
+            this.cb13.Name = "cb13";
+            this.cb13.Size = new System.Drawing.Size(27, 19);
+            this.cb13.TabIndex = 16;
+            this.cb13.Text = "13";
+            this.cb13.UseVisualStyleBackColor = true;
+            // 
+            // cb12
+            // 
+            this.cb12.AutoSize = true;
+            this.cb12.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.cb12.Location = new System.Drawing.Point(198, 242);
+            this.cb12.Name = "cb12";
+            this.cb12.Size = new System.Drawing.Size(27, 19);
+            this.cb12.TabIndex = 15;
+            this.cb12.Text = "12";
+            this.cb12.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(289, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "生成";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(393, 153);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(339, 131);
+            this.richTextBox1.TabIndex = 25;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(289, 238);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 26;
+            this.button2.Text = "清除";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 394);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cb14);
+            this.Controls.Add(this.cb13);
+            this.Controls.Add(this.cb12);
+            this.Controls.Add(this.cb11);
+            this.Controls.Add(this.cb10);
+            this.Controls.Add(this.cb9);
+            this.Controls.Add(this.cb8);
+            this.Controls.Add(this.cb7);
+            this.Controls.Add(this.cb6);
+            this.Controls.Add(this.cb5);
+            this.Controls.Add(this.cb4);
+            this.Controls.Add(this.cb3);
+            this.Controls.Add(this.cb2);
+            this.Controls.Add(this.cb1);
+            this.Controls.Add(this.cb0);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox cb0;
+        private System.Windows.Forms.CheckBox cb1;
+        private System.Windows.Forms.CheckBox cb2;
+        private System.Windows.Forms.CheckBox cb3;
+        private System.Windows.Forms.CheckBox cb7;
+        private System.Windows.Forms.CheckBox cb6;
+        private System.Windows.Forms.CheckBox cb5;
+        private System.Windows.Forms.CheckBox cb4;
+        private System.Windows.Forms.CheckBox cb11;
+        private System.Windows.Forms.CheckBox cb10;
+        private System.Windows.Forms.CheckBox cb9;
+        private System.Windows.Forms.CheckBox cb8;
+        private System.Windows.Forms.CheckBox cb14;
+        private System.Windows.Forms.CheckBox cb13;
+        private System.Windows.Forms.CheckBox cb12;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrizeType
+{
+    public partial class Form1 : Form
+    {
+        List<CheckBox> cbList = new List<CheckBox>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbList.Add(cb0);
+            cbList.Add(cb1);
+            cbList.Add(cb2);
+            cbList.Add(cb3);
+            cbList.Add(cb4);
+            cbList.Add(cb5);
+            cbList.Add(cb6);
+            cbList.Add(cb7);
+            cbList.Add(cb8);
+            cbList.Add(cb9);
+            cbList.Add(cb10);
+            cbList.Add(cb11);
+            cbList.Add(cb12);
+            cbList.Add(cb13);
+            cbList.Add(cb14);
+            //cbList.Add(cb15);
+            //cbList.Add(cb16);
+            //cbList.Add(cb17);
+            //cbList.Add(cb18);
+            //cbList.Add(cb19);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string result = "";
+            foreach (CheckBox item in cbList)
+            {
+                if (item.Checked)
+                {
+                    result += item.Text + ",";
+                }
+            }
+            result = result.TrimEnd(',');
+            richTextBox1.Text += (result + "\n");
+            foreach (CheckBox item in cbList)
+            {
+                item.Checked = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+    }
+}
